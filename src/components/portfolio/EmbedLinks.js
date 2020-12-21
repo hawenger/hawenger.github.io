f5a994b129bcb720d3bb75d7328804ab207729ef
Ameco <hamecow@gmail.com>
2020-12-21 15:32:15 -0800

    Update favicon

--- src/components/portfolio/EmbedLinks.js
+++ src/components/portfolio/EmbedLinks.js
@@ -6,7 +6,7 @@ export default function EmbedLinks(props) {
     return (
         <>
             <div style={{height:'100%', width:'100%'}}>
-            <iframe style={{height:'100%', width:'100%'}} width="560" height="315" src="https://www.youtube.com/embed/PsLynyIBWSI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe style={{height:'100%', width:'100%'}} width="560" height="315" src="https://www.youtube.com/embed/PsLynyIBWSI" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
             </div>
         </>
     )
--- src/components/portfolio/GithubButton.js
+++ src/components/portfolio/GithubButton.js
@@ -4,7 +4,7 @@ export default function GithubButton() {
 
     return (
         <>
-        <a className="GithubButton" href="https://github.com/hawenger"><i style={{color:'yellow', fontSize:'2em', backgroundColor:'aliceblue', marginTop:'.5em', marginRight:'.5em', textShadow: '-3px -1px #d7d700, 2px -4px 2px #cece6b'}} class="fa fa-github ctbtn" aria-hidden="true"></i></a>
+        <a className="GithubButton" href="https://github.com/hawenger"><i style={{color:'yellow', fontSize:'2em', backgroundColor:'aliceblue', marginTop:'.5em', marginRight:'.5em', textShadow: '-3px -1px #d7d700, 2px -4px 2px #cece6b'}} className="fa fa-github ctbtn" aria-hidden="true"></i></a>
         </>
     )
 }
