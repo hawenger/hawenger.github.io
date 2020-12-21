eda5b9c3e3b68b6a914ca8faa986fe97bbd9c3e5
Ameco <hamecow@gmail.com>
2020-12-21 15:10:02 -0800

    Update portfolio video

--- src/components/portfolio/EmbedLinks.js
+++ src/components/portfolio/EmbedLinks.js
@@ -6,8 +6,7 @@ export default function EmbedLinks(props) {
     return (
         <>
             <div style={{height:'100%', width:'100%'}}>
-                <embed src={props.link} style={{height:'100%', width:'100%'}}>
-                </embed>
+            <iframe style={{height:'100%', width:'100%'}} width="560" height="315" src="https://www.youtube.com/embed/PsLynyIBWSI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
         </>
     )
